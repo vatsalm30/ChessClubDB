f2b82e264dd77126bd4f4955a0002d927dfbb680
Vatsal Maheshwari <vatsalm30@gmail.com>
2024-08-06 08:31:41 -0400

    setup hosting on aws

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 from taipy.gui import Gui
+from taipy import Config
+
 from chess_library import ChessLibrary
 
 from pages.root import root
 from pages.games import games
 from pages.add_game import add_game
+
+
+Config.configure_global_app(websocket=True)
 
 pages = {
     "/": root,
@@ -12,4 +17,7 @@ pages = {
 }
 
 if __name__ == "__main__":
-    Gui(pages=pages, libraries=[ChessLibrary()]).run(title="Chess Club DB", dark_mode=True)
+    gui = Gui(pages=pages, libraries=[ChessLibrary()])
+    
+    gui.run(title="Chess Club DB", dark_mode=True, host="0.0.0.0", port=5000)
+
